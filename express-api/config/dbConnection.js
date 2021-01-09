fe1dd837f34513e93679eac52171aadda6c0d7e9
Saragam Ghimire <ghimire.saragam73@gmail.com>
2021-01-09 19:36:05 +0545

    thaha xaina k garey aja laptop update garna ko lagi push gardai xu

--- express-api/config/dbConnection.js
+++ express-api/config/dbConnection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const dbUrl = 'mongodb://localhost:27017/roomfinderdb';
 
-mongoose.connect(dbUrl, (err, done) => {
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+} ,(err, done) => {
     if (err) {
         console.log('Database Connection Failed');
     } else {
